bdf2e803b4a5a048e2e8bb51ee35fc2b56ccaf4c
Heatherchucr <130950564+Heatherchucr@users.noreply.github.com>
2024-08-27 19:35:33 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,10 +5,10 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href=''
+      href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
       target='_blank'
       rel='noopener noreferrer'
-      className={styles.}
+      className={styles.githubCorner}
       aria-label='View source on GitHub'
     >
       <svg
